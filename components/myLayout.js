2ab8fed51689fc37034e39b2fc3a0c0b639e6019
jinhwan <jhbae@anipen.com>
2017-08-25 16:02:33 +0900

    update

--- components/myLayout.js
+++ components/myLayout.js
@@ -1,7 +1,9 @@
 import * as React from 'react';
+import PropTypes from 'prop-types';
 import {
     AppBar,
     Divider,
+    Drawer,
     IconButton,
     List,
     ListItem,
@@ -10,17 +12,55 @@ import {
     Toolbar,
     Typography
 } from 'material-ui';
+import withStyles from 'material-ui/styles/withStyles';
 import MenuIcon from 'material-ui-icons/Menu';
 import InboxIcon from 'material-ui-icons/Inbox';
 import DraftsIcon from 'material-ui-icons/Drafts';
+import withWidth, {isWidthUp} from 'material-ui/utils/withWidth';
+import {compose} from 'recompose';
 
-export default class MyLayout extends React.Component {
+const styles = (theme) => ({
+    appBar: {
+        width: 'calc(100% - 256px)',
+        transition: 'width .2s',
+        [theme.breakpoints.down('lg')]: {
+            width: '100%'
+        },
+    },
+    nav: {
+        width: 256,
+    },
+    main: {
+        marginTop: 64,
+        float: 'left'
+    }
+});
+
+class MyLayout extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            drawerOpen: false
+        };
+        this.handleDrawerToggle = this.handleDrawerToggle.bind(this);
+        this.handleDrawerClose = this.handleDrawerClose.bind(this);
+    }
+
+    handleDrawerToggle() {
+        this.setState({drawerOpen: !this.state.drawerOpen});
+    }
+
+    handleDrawerClose() {
+        this.setState({drawerOpen: false});
+    }
+
     render() {
+        let drawerDocked = isWidthUp('md', this.props.width);
         return (
             <div>
-                <AppBar style={{width: 'calc(100% - 256px)'}}>
+                <AppBar className={this.props.classes.appBar}>
                     <Toolbar>
-                        <IconButton color="contrast" aria-label="Menu">
+                        <IconButton color="contrast" aria-label="Menu" onClick={this.handleDrawerToggle}>
                             <MenuIcon />
                         </IconButton>
                         <Typography type="title" color="inherit">
@@ -28,43 +68,46 @@ export default class MyLayout extends React.Component {
                         </Typography>
                     </Toolbar>
                 </AppBar>
-                <div style={{
-                    width: 256,
-                    float: 'left'
-                }}
-                >
-                    <List>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <InboxIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Inbox" />
-                        </ListItem>
-                        <ListItem button>
-                            <ListItemIcon>
-                                <DraftsIcon />
-                            </ListItemIcon>
-                            <ListItemText primary="Drafts" />
-                        </ListItem>
-                    </List>
-                    <Divider />
-                    <List>
-                        <ListItem button>
-                            <ListItemText primary="Trash" />
-                        </ListItem>
-                        <ListItem button component="a" href="#simple-list">
-                            <ListItemText primary="Spam" />
-                        </ListItem>
-                    </List>
-                </div>
-                <div style={{
-                    marginTop: 64,
-                    float: 'left'
-                }}
-                >
+                <Drawer anchor="left" open={drawerDocked || this.state.drawerOpen}
+                        onRequestClose={this.handleDrawerClose} docked={drawerDocked}>
+                    <div className={this.props.classes.nav}>
+                        <List>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <InboxIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Inbox" />
+                            </ListItem>
+                            <ListItem button>
+                                <ListItemIcon>
+                                    <DraftsIcon />
+                                </ListItemIcon>
+                                <ListItemText primary="Drafts" />
+                            </ListItem>
+                        </List>
+                        <Divider />
+                        <List>
+                            <ListItem button>
+                                <ListItemText primary="Trash" />
+                            </ListItem>
+                            <ListItem button component="a" href="#simple-list">
+                                <ListItemText primary="Spam" />
+                            </ListItem>
+                        </List>
+                    </div>
+                </Drawer>
+                <div className={this.props.classes.main}>
                     {this.props.children}
                 </div>
             </div>
         );
     }
 }
+
+MyLayout.propTypes = {
+    width: PropTypes.string.isRequired,
+    classes: PropTypes.object.isRequired,
+    children: PropTypes.node.isRequired
+};
+
+export default compose(withStyles(styles), withWidth())(MyLayout);
